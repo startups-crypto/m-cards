1a4c7c39dbe8c44e944f87aebd22a2375b3dfcc4
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-05-25 22:29:05 +0300

    change heder popup

--- app/components/core/header/Header.tsx
+++ app/components/core/header/Header.tsx
@@ -162,22 +162,43 @@ export default function Header({ color = '' }: HeaderProps) {
 
                 <div className="header__right_part"> 
                   <div className="s-button-launch-wrapper-reverse">
-                    <Link href={TG_BOT_URL} target="_blank" className="s-button s-button--small" rel="nonoopener nofollow">Открыть MCard</Link>
-                    <QrButton isSmall={true} color="white-transparent" clickEvent={openMenu} mouseEnter={() => {setIshover(true);}} />
+                    <a href={TG_BOT_URL} target="_blank" className="s-button s-button--small" rel="nonoopener nofollow">Открыть MCard</a>
+                    {/* <QrButton isSmall={true} color="white-transparent" clickEvent={openMenu} mouseEnter={() => {setIshover(true);}} /> */}
+
+                    <button className="qr-button qr-button--small white-transparent" onClick={openMenu} onMouseEnter={() => {setIshover(true);}}>
+                      <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <mask id="mask0_263_49" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="35" height="35">
+                          <rect width="35" height="35" fill="#D9D9D9"/>
+                          </mask>
+                          <g mask="url(#mask0_263_49)">
+                          <path className="hover-path" d="M5.10352 16.0416V5.10413H16.041V16.0416H5.10352ZM7.29102 13.8541H13.8535V7.29163H7.29102V13.8541ZM5.10352 29.8958V18.9583H16.041V29.8958H5.10352ZM7.29102 27.7083H13.8535V21.1458H7.29102V27.7083ZM18.9577 16.0416V5.10413H29.8952V16.0416H18.9577ZM21.1452 13.8541H27.7077V7.29163H21.1452V13.8541ZM27.1608 29.8958V27.1614H29.8952V29.8958H27.1608ZM18.9577 21.6927V18.9583H21.6921V21.6927H18.9577ZM21.6921 24.427V21.6927H24.4264V24.427H21.6921ZM18.9577 27.1614V24.427H21.6921V27.1614H18.9577ZM21.6921 29.8958V27.1614H24.4264V29.8958H21.6921ZM24.4264 27.1614V24.427H27.1608V27.1614H24.4264ZM24.4264 21.6927V18.9583H27.1608V21.6927H24.4264ZM27.1608 24.427V21.6927H29.8952V24.427H27.1608Z" fill="black"/>
+                          </g>
+                      </svg>
+                      <div className="qr-button-hover-wrapper qr-button-hover-wrapper--bottom">
+                          <div>
+                              <div className="qr-code">
+                                  <img src="/svg/qr-code-hover.svg" alt="qr code" title="qr-code" width="132" height="132" loading="lazy" decoding="async"/>
+                              </div>
+                              <div className="qr-code-social">
+                                  <Socials />
+                              </div>
+                          </div>
+                      </div>
+                    </button>
+
                   </div>
                 </div>
             </div>
-            <div className="header-hover" onMouseLeave={() => {setIshover(false);}}>
+            {/* <div className="header-hover" onMouseLeave={() => {setIshover(false);}}>
               <div className="header-hover__wrapper">
                   <div className="header-qr-code">
                     <img src="/svg/header-qr-code.svg" alt="Header qr code" width="150" height="150" loading="lazy" decoding="async" />
                   </div>
-                  <div className="header-divider"></div>
                   <div className="header-socials">
                     <Socials />
                   </div>
               </div>
-            </div>
+            </div> */}
           </div>
       </header>
     </>
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -15,7 +15,6 @@ export default function Hero() {
     useEffect(() => {
         const hero = heroRef.current;
         
-
         if (!hero) {
             return;
         }
